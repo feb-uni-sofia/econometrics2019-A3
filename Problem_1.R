b44d16a833d46311028a348d72a42bcdfb567f91
Boyko Amarov <boyko.amarov@gmail.com>
2019-04-10 11:31:26 +0300

    Update Problem_1.R

--- Problem_1.R
+++ Problem_1.R
@@ -1,7 +1,69 @@
-## Problem set 3, Problem 1
-## Problem description:
-## https://firebasestorage.googleapis.com/v0/b/uni-sofia.appspot.com/o/assignments%2FA3.pdf?alt=media
 
-# a)
-childiq <- read.csv('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/childiq.csv')
-str(childiq)
+## Problem set 3
+## Problem 2
+
+## a)
+xgrid <- seq(from = -6, to = 6, by =0.2)
+
+## X ~ N(0, 1)
+## Y ~ N(1, 2)
+
+## b, c and d)
+## Computes the density function of the standard normal
+## distribution for each value in xgrid
+
+densityX <- dnorm(xgrid, mean = 0, sd = 1)
+
+## Computes the value of the density function of
+## a normal distribution with mean 1 and standard
+## deviation sqrt(2) (i.e. variance equal to 2)
+
+densityY <- dnorm(xgrid, mean = 1, sd = sqrt(2))
+
+## Create an empty plot
+plot(
+  xgrid,
+  densityX,
+  type = "n"
+)
+
+
+## Draw lines in the plot with
+## x-coordinates equal to xgrid and y-coordinates
+## equal to densityX
+lines(
+  xgrid, 
+  densityX
+)
+
+## Draw lines for the density of Y
+lines(
+  xgrid, 
+  densityY,
+  col = "red"
+)
+
+## e)
+
+## P(X < -1)
+pnorm(-1)
+
+## P(Y > 2)
+1 - pnorm(2, mean = 1, sd = sqrt(2))
+
+## P(-1 < X < 1)
+pnorm(1) - pnorm(-1)
+
+## f)
+## P(X < ???) = 0.05
+qnorm(0.05)
+
+## P(Y < ???) = 0.05
+qnorm(0.05, mean = 1, sd = sqrt(2))
+
+## g)
+## P(X < ???) = 0.975
+qnorm(0.975)
+
+## P(Y < ???) = 0.975
+qnorm(0.975, mean = 1, sd = sqrt(2))
