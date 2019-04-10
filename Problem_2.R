0b820a155333e8ef8ee4d5297e691d2ec3c41886
Boyko Amarov <boyko.amarov@gmail.com>
2019-04-10 11:31:40 +0300

    Update Problem_2.R

--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,8 @@
+## Problem set 3, Problem 1
+## Problem description:
+## https://firebasestorage.googleapis.com/v0/b/uni-sofia.appspot.com/o/assignments%2FA3.pdf?alt=media
+
+# a)
+childiq <- read.csv('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/childiq.csv')
+str(childiq)
 
