78546a8e51402067c3f7ef79ac1639471145b3e6
Boyko Amarov <boyko.amarov@gmail.com>
2019-05-14 10:30:52 +0300

    Create Problem_4.R

---
+++ Problem_4.R
@@ -0,0 +1,3 @@
+## Read the data
+
+inc <- read.csv('https://firebasestorage.googleapis.com/v0/b/uni-sofia.appspot.com/o/courses%2Feconometrics2019%2Fassignments%2Fassignment4%2Fincome_and_education_sim.csv?alt=media&token=71af68ad-1ea2-4cb6-9c2c-fa711b5ba9a9')
